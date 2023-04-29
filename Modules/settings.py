5e7f5db9e2f2aecb538cdc2452dd2c0f6e652cc6
Jeremy Munsell <jeremymunsell@gmail.com>
2023-04-29 09:15:11 -0500

    added requirements.txt

--- Modules/settings.py
+++ Modules/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-nkfpno@(#=m91e=f+h=g6aa7at_+c#2cjxsm3*xk3x*oeph)le
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
