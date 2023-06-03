0b13927ede061ae14da50b2cf79fcb21ece084bc
Jeremy Munsell <jeremy.munsell.2022@gmail.com>
2023-06-03 18:53:47 -0500

    completed data download api

--- download/urls.py
+++ download/urls.py
@@ -4,5 +4,16 @@ from . import views
 
 app_name = 'download'
 urlpatterns = [
-    path('', views.download_view, name='dl'),
+    path('', views.download_api, name='main'),
+    path('cal/', views.cal_download_view, name='cal'),
+    path('user/', views.user_download_view, name='user'),
+    path('pre_edu/', views.preEDU_download_view, name='pre_edu'),
+    path('pre_log/', views.preLOG_download_view, name='pre_log'),
+    path('pre_mouse/', views.preMouse_download_view, name='pre_mouse'),
+    path('force_edu/', views.forceEDU_download_view, name='force_edu'),
+    path('force_log/', views.forceLOG_download_view, name='force_log'),
+    path('force_mouse/', views.forceMouse_download_view, name='force_mouse'),
+    path('post_edu/', views.forceEDU_download_view, name='post_edu'),
+    path('post_log/', views.forceLOG_download_view, name='post_log'),
+    path('post_mouse/', views.forceMouse_download_view, name='post_mouse'),
 ]
--- download/views.py
+++ download/views.py
@@ -1,25 +1,181 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import calibrate.models as cal
+from calibrate.models import clicks
 import Force_HLG.models as force
 import Login.models as log
+from django.contrib.auth.models import User
 from csv import writer
 
-# Create your views here.
-def download_view(request):
+#### Calibrate ###########################################
+def cal_download_view(request):
     
-    obj = cal.objects.all()
-    response = HttpResponse(
-    content_type="text/csv",
-    headers={"Content-Disposition": 'attachment; filename="calibrate.csv"'},
+    obj = clicks.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="calibrate.csv"'}
     )
 
-    print(obj)
+    csv_writer = writer(response)
+    csv_writer.writerow(["user","mouseX","mouseY","time_stamp","event"])
+    for row in obj:
+        csv_writer.writerow([row.user, row.mouseX, row.mouseY, row.time_stamp, row.event])
 
-    writer = csv.writer(response)
-    writer.writerow(["user","mouseX","mouseY","time_stamp","event"])
-    writer.writerow(["Second row", "A", "B", "C", '"Testing"', "Here's a quote"])
+    return response
+
+
+## Pretest ###############################################
+
+
+def preEDU_download_view(request):
+    
+    obj = force.pretestEdu.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="pretestEDU.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","timeStamp","user","question","choice"])
+    for row in obj:
+        csv_writer.writerow([row.pageState, row.timeStamp, row.user, row.question, row.choice])
 
     return response
 
+def preLOG_download_view(request):
+    
+    obj = force.pretestLog.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="prettestLog.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","VideoNumber","videoTime","timeStamp","wandering"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.videoNumber,row.timeStamp,row.wandering])
+
     return response
+
+def preMouse_download_view(request):
+    
+    obj = force.pretestMouseEvent.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="pretestMouse.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","timeStamp","mouseX","mouseY","clickedITMtl","clickedITMbr","keyPressed"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.timeStamp,row.mouseX,row.mouseY,row.clickedITMtl,row.clickedITMbr,row.keyPressed])
+
+    return response
+
+
+## force/instructional #############################################
+
+
+def forceEDU_download_view(request):
+    
+    obj = force.forceEDU.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="forceEDU.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","timeStamp","question","choice"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.timeStamp,row.question,row.choice])
+
+    return response
+
+def forceLOG_download_view(request):
+    
+    obj = force.forceLog.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="forceLog.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","VideoNumber","videoTime","timeStamp","wandering"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.videoNumber,row.timeStamp,row.wandering])
+
+    return response
+
+def forceMouse_download_view(request):
+    
+    obj = force.forceMouseEvent.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="forceMouse.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","timeStamp","mouseX","mouseY","clickedITMtl","clickedITMbr","keyPressed"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.timeStamp,row.mouseX,row.mouseY,row.clickedITMtl,row.clickedITMbr,row.keyPressed])
+
+    return response
+
+
+## Post-Test #########################################################3
+
+
+def postEDU_download_view(request):
+    
+    obj = force.posttestEdu.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="posttestEDU.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","timeStamp","user","question","choice"])
+    for row in obj:
+        csv_writer.writerow([row.pageState, row.timeStamp, row.user, row.question, row.choice])
+
+    return response
+
+def postLOG_download_view(request):
+    
+    obj = force.posttestLog.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="posttestLog.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","VideoNumber","videoTime","timeStamp","wandering"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.videoNumber,row.timeStamp,row.wandering])
+
+    return response
+
+def postMouse_download_view(request):
+    
+    obj = force.posttestMouseEvent.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="posttestMouse.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["pageState","event","user","timeStamp","mouseX","mouseY","clickedITMtl","clickedITMbr","keyPressed"])
+    for row in obj:
+        csv_writer.writerow([row.pageState,row.event,row.user,row.timeStamp,row.mouseX,row.mouseY,row.clickedITMtl,row.clickedITMbr,row.keyPressed])
+
+    return response
+
+## User model ########################################33
+
+from django.contrib.auth.models import User
+
+def user_download_view(request):
+    
+    obj = User.objects.all()
+    response = HttpResponse(content_type="text/csv",
+    headers={"Content-Disposition": 'attachment; filename="users.csv"'}
+    )
+
+    csv_writer = writer(response)
+    csv_writer.writerow(["username","email_address","first_name","last_name","staff"])
+    for row in obj:
+        csv_writer.writerow([row.username,row.email,row.first_name,row.last_name,row.is_staff])
+
+    return response
+
+def download_api(request):
+    return render(request,"download/data_download.html")
